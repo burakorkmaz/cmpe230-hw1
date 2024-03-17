fb0ef5204c099ca2b2b281ac45cd20017a45a794
burakorkmaz <kkorkmazburakk@gmail.com>
2024-03-17 15:08:59 +0300

    Add reading functions, add terminal.c, change main.c

---
+++ terminal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void readTerminal(char *buffer, int size) {
+    printf(">> ");
+    scanf("%[^\n]", buffer);
+}
+
+void tokenize(char *buffer, char *delim, char **tokens, int *numTokens) {
+    char *token = strtok(buffer, delim);
+    int i = 0;
+    while (token != NULL) {
+        tokens[i] = token;
+        token = strtok(NULL, delim);
+        i++;
+    }
+    *numTokens = i;
+}
